8af799acc55763573d54ff871e757f021340e846
Elion05 <elion.rexborn@gmail.com>
2024-10-03 11:26:53 +0200

    oefeningen

--- week2/lussen.mjs
+++ week2/lussen.mjs
@@ -2,9 +2,8 @@ import * as readline from 'node:readline/promises';
 import{stdin as input, stdout as output} from 'node:process';
 const userInput = readline.createInterface({input, output});
 
-let getalw = 3
-
-while(getalw != 1){
-    console.log(getalw);
-getalw += 3
+let getalP = 1
+while(getalP <= 99){
+    console.log(getalP)
+getalP += 1
 }
