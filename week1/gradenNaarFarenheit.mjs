6201335566a0f1d623439e31a9effdcedcd10dd6
Elion05 <elion.rexborn@gmail.com>
2024-10-11 17:37:58 +0200

    oefeningen

--- week1/gradenNaarFarenheit.mjs
+++
@@ -1,8 +0,0 @@
-import * as readline from 'node:readline/promises';
-import{stdin as input, stdout as output} from 'node:process';
-const userInput = readline.createInterface({input, output});
-
-let gradenCelcius = await userInput.question('hoeveel graden Celcius is het? ');
-console.log(gradenCelcius);
-let gradenFarenheit = (gradenCelcius * 9 / 5) + 32;
-console.log(' het is ' + gradenFarenheit + ' Farenheit ');
--- week2/lussen.mjs
+++
@@ -1,9 +0,0 @@
-import * as readline from 'node:readline/promises';
-import{stdin as input, stdout as output} from 'node:process';
-const userInput = readline.createInterface({input, output});
-
-let getalP = 1
-while(getalP <= 99){
-    console.log(getalP)
-getalP += 1
-}
